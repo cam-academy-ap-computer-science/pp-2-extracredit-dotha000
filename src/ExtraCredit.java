a11f43484f1c38d78a01a04d4310a848abe90144
do_tha000 <do_tha000@CAM-LAB-23.bgsd.k12.wa.us>
2019-10-14 07:51:34 -0700

    NEW SAVE 1

--- src/ExtraCredit.java
+++ src/ExtraCredit.java
@@ -1,8 +1,16 @@
 
 public class ExtraCredit {
-
+	public static final int STEPS = 5;
 	public static void main(String[] args) {
-
+		//CREATE HEAD SPACES
+		for(int i = 1; i <= 5 * (STEPS - 1) + 2; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("o  *****");
+		//CREATE BODY SPACES
+		for(int i = 1; i <= 5 * (STEPS - 1) + 1; i++) {
+			System.out.print(" ");
+		}
+		System.out.print
 	}
-
 }
